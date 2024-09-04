820433a152599582b9b0415e3ed852cffc1dbc61
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-09-05 01:10:55 +0900

    Design: UI 재구성

--- src/pages/Group/index.js
+++ src/pages/Group/index.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./styles.css";
-import profileLine from "../../assets/group/profile_line.png";
-import memoryUpload from "../../assets/group/추억올리기.png";
-import toggleButton from "../../assets/group/공개비공개버튼.png";
 import publicPost from "../../assets/group/공개글1.png";
 import publicPost2 from "../../assets/group/공개글2.png";
 import publicPost3 from "../../assets/group/공개글3.png";
@@ -11,11 +8,9 @@ import publicPost4 from "../../assets/group/공개글4.png";
 import publicPost5 from "../../assets/group/공개글5.png";
 import moreButton from "../../assets/group/더보기.png";
 import privatePost from "../../assets/group/비공개글.png";
-import sympathyButton from "../../assets/group/공감보내기.png";
-import sympathyAnimation from "../../assets/group/공감보내기클릭시.png";
-import badge1 from "../../assets/group/badge1.png";
-import badge2 from "../../assets/group/badge2.png";
-import badge3 from "../../assets/group/badge3.png";
+import sendLike from "../../assets/group/sendLike.svg";
+import likeAnimation from "../../assets/group/likeAnimation.svg";
+import searchIcon from "../../assets/group/searchIcon.svg";
 
 // 새로운 이미지 import
 import groupPhoto from "../../assets/group/그룹사진.png";
@@ -45,7 +40,7 @@ const Group = () => {
   useEffect(() => {
     const fetchGroupInfo = () => {
       setName("달봉이네 가족");
-      setImageUrl();
+      setImageUrl(groupPhoto);
       setIsPublic(false);
       setIntroduction("서로 한 마음으로 응원하고 아끼는 달봉이네 가족입니다.");
       setLikeCount(8);
@@ -97,11 +92,19 @@ const Group = () => {
   return (
     <>
       <LogoTopBar />
+
+      {/* 그룹 정보 수정 모달 */}
+      {showModal && <UpdateGroupModal handleCloseModal={handleCloseModal} />}
+
+      {/* 그룹 삭제 모달 */}
+      {showDeleteModal && (
+        <DeleteGroupModal handleCloseModal={handleCloseDeleteModal} />
+      )}
       <div className="home-container">
         <div className="info-container">
-          <img src={groupPhoto} alt="그룹 사진" className="group-image" />
+          <img src={imageUrl} alt="그룹 사진" className="group-image" />
           <div className="info-content">
-            <div className="container">
+            <div className="top-container">
               <div className="dday">{`D+${dday}`}</div>
               <div className="status-public">
                 |&nbsp;&nbsp;&nbsp;{isPublic ? "공개" : "비공개"}
@@ -122,31 +125,31 @@ const Group = () => {
               </div>
             </div>
 
-            <div className="container">
+            <div className="title-container">
               <span className="group-name">{name}</span>
-              <span className="memory-count">{`추억 ${postCount}`}</span>
-              <span className="group-sympathy">
-                |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span className="post-count">{`추억 ${postCount}`}</span>
+              <span className="like-count">
+                |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 {`그룹 공감 ${likeCount}`}
               </span>
             </div>
 
-            <div className="container">
-              <p className="profile-description">{introduction}</p>
-            </div>
+            <p className="introduction">{introduction}</p>
 
             <div className="badge-container">
               <span className="badge-text">획득 배지</span>
-              <div className="bage-list">
-                <img src={badge1} alt="Badge 1" className="badge" />
-                <img src={badge2} alt="Badge 2" className="badge" />
-                <img src={badge3} alt="Badge 2" className="badge" />
+              <div className="badge-list">
+                {badges.map((badge, index) => (
+                  <div key={index} className="badge-item">
+                    <span>{badge}</span>
+                  </div>
+                ))}
               </div>
             </div>
 
             {/* 공감 보내기 버튼 */}
             <img
-              src={sympathyButton}
+              src={sendLike}
               alt="공감 보내기 버튼"
               className="sympathy-button"
               onClick={handleSympathyClick}
@@ -155,35 +158,33 @@ const Group = () => {
             {/* 공감 애니메이션 */}
             {showSympathy && (
               <img
-                src={sympathyAnimation}
+                src={likeAnimation}
                 alt="공감 애니메이션"
-                className="sympathy-animation"
+                className="like-animation"
               />
             )}
           </div>
         </div>
 
-        {/* 그룹 정보 수정 모달 */}
-        {showModal && <UpdateGroupModal handleCloseModal={handleCloseModal} />}
-
-        {/* 그룹 삭제 모달 */}
-        {showDeleteModal && (
-          <DeleteGroupModal handleCloseModal={handleCloseDeleteModal} />
-        )}
-
         <div className="posts-container">
-          <div className="container">
+          <div className="post-top-container">
             <span className="post-text">추억 목록</span>
             <button onClick={handleUploadClick} className="post-upload-btn">
               추억 올리기
             </button>
           </div>
 
-          <div className="container">
-            <button className="public-button" onClick={handleShowPublic}>
+          <div className="post-mid-container">
+            <button
+              className={`public-button ${isPublicView ? "active" : ""}`}
+              onClick={handleShowPublic}
+            >
               공개
             </button>
-            <button className="public-button" onClick={handleShowPrivate}>
+            <button
+              className={`public-button ${!isPublicView ? "active" : ""}`}
+              onClick={handleShowPrivate}
+            >
               비공개
             </button>
             <input
@@ -211,7 +212,7 @@ const Group = () => {
               <img src={privatePost} alt="비공개글5" className="post" />
             </div>
           )}
-          <img src={moreButton} alt="더보기 버튼" className="more-button" />
+          <button className="load-more-btn">더보기</button>
         </div>
       </div>
     </>
