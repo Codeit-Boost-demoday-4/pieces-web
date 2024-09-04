f586f5acefda238c9aa69b93f8337690ef1eb715
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-09-04 22:52:38 +0900

    Design: fixed 수정

--- src/pages/Group/index.js
+++ src/pages/Group/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./styles.css";
-import logo from "../../assets/logo.png";
 import profileLine from "../../assets/group/profile_line.png";
 import memoryUpload from "../../assets/group/추억올리기.png";
 import toggleButton from "../../assets/group/공개비공개버튼.png";
@@ -21,6 +20,7 @@ import badge3 from "../../assets/group/badge3.png";
 // 새로운 이미지 import
 import groupPhoto from "../../assets/group/그룹사진.png";
 
+import { LogoTopBar } from "../../components/LogoTopBar";
 import UpdateGroupModal from "./UpdateGroupModal";
 import DeleteGroupModal from "./DeleteGroupModal";
 
@@ -32,24 +32,34 @@ const Group = () => {
   const [showModal, setShowModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  /*
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPublic, setIsPublic] = useState(true);
+  const [introduction, setIntroduction] = useState("");
+  const [likeCount, setLikeCount] = useState(0);
+  const [badges, setBadges] = useState([]);
+  const [postCount, setPostCount] = useState(0);
+  const [dday, setDDay] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
-    const scalePage = () => {
-      const scale = window.innerWidth / 1920; // 1920px 기준으로 스케일 계산
-      document.body.style.transform = `scale(${scale})`;
-      document.body.style.transformOrigin = "top left"; // 기준점 설정
-      document.body.style.width = "1920px"; // 축소 후 너비 고정
-      document.body.style.height = "auto"; // 필요시 자동 높이 설정
+    const fetchGroupInfo = () => {
+      setName("달봉이네 가족");
+      setImageUrl();
+      setIsPublic(false);
+      setIntroduction("서로 한 마음으로 응원하고 아끼는 달봉이네 가족입니다.");
+      setLikeCount(8);
+      setBadges([
+        "7일 연속 추억 등록",
+        "그룹 공감 1만 개 이상 받기",
+        "추억 공감 1만 개 이상 받기",
+      ]);
+      setPostCount(8);
+      setDDay(265);
     };
 
-    scalePage(); // 페이지 로드 시 스케일 조정
-    window.addEventListener("resize", scalePage); // 창 크기 조정 시 스케일 재적용
-
-    return () => {
-      window.removeEventListener("resize", scalePage);
-    };
+    fetchGroupInfo();
   }, []);
-  */
 
   const handleShowPublic = () => {
     setIsPublicView(true);
@@ -85,153 +95,126 @@ const Group = () => {
   };
 
   return (
-    <div className="home-container">
-      <div className="background-layer"></div>
-      <img src={logo} alt="로고" className="logo" />
-      <img src={profileLine} alt="프로필 선" className="profile-line" />
-      <span className="memory-list">추억 목록</span>
+    <>
+      <LogoTopBar />
+      <div className="home-container">
+        <div className="info-container">
+          <img src={groupPhoto} alt="그룹 사진" className="group-image" />
+          <div className="info-content">
+            <div className="container">
+              <div className="dday">{`D+${dday}`}</div>
+              <div className="status-public">
+                |&nbsp;&nbsp;&nbsp;{isPublic ? "공개" : "비공개"}
+              </div>
+              <div
+                className="edit-button"
+                onClick={handleTextClick}
+                style={{ cursor: "pointer" }}
+              >
+                그룹 정보 수정
+              </div>
+              <div
+                className="delete-button"
+                onClick={handleDeleteClick}
+                style={{ cursor: "pointer" }}
+              >
+                그룹 삭제하기
+              </div>
+            </div>
 
-      <button onClick={handleUploadClick}>
-        <img src={memoryUpload} alt="추억 올리기" className="memory-upload" />
-      </button>
+            <div className="container">
+              <span className="group-name">{name}</span>
+              <span className="memory-count">{`추억 ${postCount}`}</span>
+              <span className="group-sympathy">
+                |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                {`그룹 공감 ${likeCount}`}
+              </span>
+            </div>
 
-      <img
-        src={toggleButton}
-        alt="공개 비공개 버튼"
-        className="toggle-button"
-      />
-      <span
-        className="public-text"
-        onClick={handleShowPublic}
-        style={{ cursor: "pointer" }}
-      >
-        공개
-      </span>
+            <div className="container">
+              <p className="profile-description">{introduction}</p>
+            </div>
 
-      <span
-        className="private-text"
-        onClick={handleShowPrivate}
-        style={{ cursor: "pointer" }}
-      >
-        비공개
-      </span>
+            <div className="badge-container">
+              <span className="badge-text">획득 배지</span>
+              <div className="bage-list">
+                <img src={badge1} alt="Badge 1" className="badge" />
+                <img src={badge2} alt="Badge 2" className="badge" />
+                <img src={badge3} alt="Badge 2" className="badge" />
+              </div>
+            </div>
 
-      {/* 그룹 정보 수정 버튼 */}
-      <div
-        className="text-button"
-        onClick={handleTextClick}
-        style={{ cursor: "pointer" }}
-      >
-        그룹 정보 수정
-      </div>
+            {/* 공감 보내기 버튼 */}
+            <img
+              src={sympathyButton}
+              alt="공감 보내기 버튼"
+              className="sympathy-button"
+              onClick={handleSympathyClick}
+            />
 
-      <div className="dday-description">D+265</div>
+            {/* 공감 애니메이션 */}
+            {showSympathy && (
+              <img
+                src={sympathyAnimation}
+                alt="공감 애니메이션"
+                className="sympathy-animation"
+              />
+            )}
+          </div>
+        </div>
 
-      <div className="status-public">|&nbsp;&nbsp;&nbsp;공개</div>
+        {/* 그룹 정보 수정 모달 */}
+        {showModal && <UpdateGroupModal handleCloseModal={handleCloseModal} />}
 
-      <div className="memory-count">추억 8</div>
+        {/* 그룹 삭제 모달 */}
+        {showDeleteModal && (
+          <DeleteGroupModal handleCloseModal={handleCloseDeleteModal} />
+        )}
 
-      <div className="group-sympathy">
-        |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;그룹
-        공감 23
-      </div>
+        <div className="posts-container">
+          <div className="container">
+            <span className="post-text">추억 목록</span>
+            <button onClick={handleUploadClick} className="post-upload-btn">
+              추억 올리기
+            </button>
+          </div>
 
-      {/* 그룹 삭제하기 버튼 */}
-      <div
-        className="delete-button"
-        onClick={handleDeleteClick}
-        style={{ cursor: "pointer" }}
-      >
-        그룹 삭제하기
-      </div>
+          <div className="container">
+            <button className="public-button" onClick={handleShowPublic}>
+              공개
+            </button>
+            <button className="public-button" onClick={handleShowPrivate}>
+              비공개
+            </button>
+            <input
+              className="search-input"
+              type="text"
+              value={searchQuery}
+              placeholder="태그 혹은 제목을 입력해주세요"
+            />
+          </div>
 
-      <div className="group-name">달봉이네 가족</div>
-
-      {/* 공감 보내기 버튼 */}
-      <img
-        src={sympathyButton}
-        alt="공감 보내기 버튼"
-        className="sympathy-button"
-        onClick={handleSympathyClick}
-      />
-
-      <img src={badge1} alt="Badge 1" className="badge-image-1" />
-      <img src={badge2} alt="Badge 2" className="badge-image-2" />
-      <img src={badge3} alt="Badge 2" className="badge-image-3" />
-
-      {/* 공감 애니메이션 */}
-      {showSympathy && (
-        <img
-          src={sympathyAnimation}
-          alt="공감 애니메이션"
-          className="sympathy-animation"
-        />
-      )}
-
-      {/* 입력창 모달 */}
-      {showModal && <UpdateGroupModal handleCloseModal={handleCloseModal} />}
-
-      {/* 그룹 삭제 모달 */}
-      {showDeleteModal && (
-        <DeleteGroupModal handleCloseModal={handleCloseDeleteModal} />
-      )}
-
-      {isPublicView ? (
-        <>
-          <img src={publicPost} alt="공개글1" className="public-post" />
-          <img src={publicPost2} alt="공개글2" className="public-post-2" />
-          <img src={publicPost3} alt="공개글3" className="public-post-3" />
-          <img src={publicPost4} alt="공개글4" className="public-post-4" />
-          <img src={publicPost5} alt="공개글5" className="public-post-5" />
+          {isPublicView ? (
+            <div className="posts-list">
+              <img src={publicPost} alt="공개글1" className="post" />
+              <img src={publicPost2} alt="공개글2" className="post" />
+              <img src={publicPost3} alt="공개글3" className="post" />
+              <img src={publicPost4} alt="공개글4" className="post" />
+              <img src={publicPost5} alt="공개글5" className="post" />
+            </div>
+          ) : (
+            <div className="posts-list">
+              <img src={privatePost} alt="비공개글1" className="post" />
+              <img src={privatePost} alt="비공개글2" className="post" />
+              <img src={privatePost} alt="비공개글3" className="post" />
+              <img src={privatePost} alt="비공개글4" className="post" />
+              <img src={privatePost} alt="비공개글5" className="post" />
+            </div>
+          )}
           <img src={moreButton} alt="더보기 버튼" className="more-button" />
-        </>
-      ) : (
-        <>
-          <img
-            src={privatePost}
-            alt="비공개글1"
-            className="private-post-group1-1"
-          />
-          <img
-            src={privatePost}
-            alt="비공개글2"
-            className="private-post-group1-2"
-          />
-          <img
-            src={privatePost}
-            alt="비공개글3"
-            className="private-post-group1-3"
-          />
-          <img
-            src={privatePost}
-            alt="비공개글4"
-            className="private-post-group1-4"
-          />
-
-          <img
-            src={privatePost}
-            alt="비공개글5"
-            className="private-post-group2-1"
-          />
-
-          <img
-            src={moreButton}
-            alt="더보기 버튼"
-            className="more-button-private"
-          />
-        </>
-      )}
-      {/* "달봉이네 가족" 문구 추가 */}
-      <p className="profile-description">
-        서로 한 마음으로 응원하고 아끼는 달봉이네 가족입니다.
-      </p>
-
-      {/* "획득 배지" 문구 추가 */}
-      <p className="badge-text">획득 배지</p>
-
-      {/* 추가된 이미지 */}
-      <img src={groupPhoto} alt="그룹 사진" className="group-photo" />
-    </div>
+        </div>
+      </div>
+    </>
   );
 };
 
